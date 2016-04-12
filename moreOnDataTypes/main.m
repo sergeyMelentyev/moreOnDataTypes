5da8b8bb1cb48e840d29cc23583bbe2fbbfa3af5
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-04-12 12:18:22 +0300

    new version

---
+++ moreOnDataTypes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  moreOnDataTypes
+//
+//  Created by Админ on 12.04.16.
+//  Copyright © 2016 Melentyev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return 0;
+}
